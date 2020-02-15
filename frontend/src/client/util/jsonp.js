9fb6a7b4517b69b3d2215be1f663087268f06ac0
arne7morgen <asiebenm@iavgroup.local>
2020-02-15 21:10:50 +0100

    added frontend

---
+++ frontend/src/client/util/jsonp.js
@@ -0,0 +1,12 @@
+export default function jsonp(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(1000000 * Math.random());
+    window[callbackName] = function (data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
