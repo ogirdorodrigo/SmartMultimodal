5cd372986829cc484ee0921a35ed3df0253de8c9
arne7morgen <asiebenm@iavgroup.local>
2020-02-15 15:44:25 +0100

    Added new API key

--- backend/server.py
+++ backend/server.py
@@ -3,9 +3,11 @@ import googlemaps
 from datetime import datetime
 import polyline
 
+with open('backend/google_key.txt', 'r') as f:
+    google_key = f.read()
 
 app = Flask(__name__)
-gmaps = googlemaps.Client(key='AIzaSyCypPj6yAIQ2g2Iu9X0qpGEMSw6Vzw-x0Y')
+gmaps = googlemaps.Client(key=google_key)
 
 @app.route('/')
 def hello_world():
@@ -17,11 +19,12 @@ def get_polyline_from_direction(direction):
 
 if __name__ == "__main__":
     now = datetime.now()
-    directions_result = gmaps.directions("Tempelhoferfeld",
-                                     "Alexanderplatz",
+    
+    directions_result = gmaps.directions((52.52103409999999,13.4127321),
+                                     "TU Berlin",
                                      mode="transit",
                                      departure_time=now)
-    #print(directions_result)
+    print(directions_result)
     points = get_polyline_from_direction(directions_result[0])
     #app.run(debug=True)
     
